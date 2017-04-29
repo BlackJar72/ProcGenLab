882f32c4829d63a727f3e40fcf7f311130c716ef
Jared Blackburn (JaredBGreat) <BlackJar72@yahoo.com>
2017-04-29 03:46:13 -0400

    (minor)

--- src/jaredbgreat/procgenlab/api/util/SpatialNoise.java
+++ src/jaredbgreat/procgenlab/api/util/SpatialNoise.java
@@ -1,7 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+  * Copyright (C) Jared Blackburn 2017
+- *
+- * Currently under the Creative Commons Attribution License version 4.0:  
+- * https://creativecommons.org/licenses/by/4.0/legalcode
  */
 package jaredbgreat.procgenlab.api.util;
 
